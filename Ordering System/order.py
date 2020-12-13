620138a658c32e08cc85083c2e522f8ba1953e09
hetshah99 <hetshah25@gmail.com>
2020-12-13 08:57:42 +0530

    Ordering System

---
+++ Ordering System/order.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  6 11:11:48 2020
+
+@author: HETSHAH
+"""
+
+from flask import Flask,request, redirect,render_template
+import psycopg2
+import pika, os
+import time
+import json as JSON
+import requests
+
+
+app = Flask(__name__)
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+
+    if request.method == "POST":
+
+        req = request.form
+        print(req)
+        connection = psycopg2.connect(user="postgres",
+                                  password="het",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="postgres")
+        cursor = connection.cursor()
+        cursor.execute("ROLLBACK")
+        
+        
+        json = JSON.dumps(req);
+        print(json)
+        temp =requests.get('http://127.0.0.1:8000/order')
+        time.sleep(2)
+        temp =requests.get('http://127.0.0.1:8000/order')
+        
+        
+        #RABBITMQ PUBLISH QUEUE1
+        url = os.environ.get('CLOUDAMQP_URL', 'amqps://ugsynvns:zl-xQ-7yv9OHkXk9s7BQizgUBGBSzTff@fox.rmq.cloudamqp.com/ugsynvns')
+        params = pika.URLParameters(url)
+        connection2 = pika.BlockingConnection(params)
+        channel = connection2.channel()
+        channel.queue_declare(queue='queue1')
+        
+        channel.basic_publish(exchange='',
+                              routing_key='queue1',
+                              body=json
+                              )
+        connection2.close()
+        
+        #RABBITMQ SUBSCRIBE QUEUE2
+        time.sleep(15)
+        url = os.environ.get('CLOUDAMQP_URL', 'amqps://ugsynvns:zl-xQ-7yv9OHkXk9s7BQizgUBGBSzTff@fox.rmq.cloudamqp.com/ugsynvns')
+        params = pika.URLParameters(url)
+        connection2 = pika.BlockingConnection(params)
+        channel = connection2.channel()
+        channel.queue_declare(queue='queue2')
+        
+        method_frame, header_frame, body = channel.basic_get(queue = 'queue2')
+        print(body)
+        print("Status about your order : ", body)      
+        channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+        connection2.close()
+        body= body.strip()
+        postgres_insert_query = """ INSERT INTO bookorder (bid,qty,status ) VALUES (%s,%s,%s)"""
+        record_to_insert = (req['bid'],req['quant'],str(body))
+        cursor.execute(postgres_insert_query, record_to_insert)
+        connection.commit()
+        
+        if(connection):
+            cursor.close()
+            connection.close()
+
+        return render_template("sign_up1.html")
+            
+    return render_template("sign_up.html")
+
+@app.route('/orderinfo')
+def v_timestamp():
+    connection = psycopg2.connect(user="postgres",
+                                  password="het",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="postgres")
+    mycursor = connection.cursor()
+    mycursor.execute("SELECT * FROM bookorder")
+    data = mycursor.fetchall()
+    return render_template('orderinfo.html', data=data)
+if __name__ == '__main__':
+    app.run(debug=False, port=8080)
---
+++ Ordering System/stock.py
@@ -0,0 +1,85 @@
+import psycopg2
+import pika, os
+import json
+
+
+from flask import Flask,request, redirect,render_template
+import psycopg2
+import pika, os
+import time
+import json as json1
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="book management"
+)
+mycursor = mydb.cursor()
+
+app = Flask(__name__)
+
+@app.route("/order", methods=["GET"])
+def order():
+    
+    print("hi")
+    #RABBITMQ SUBSCRIBE QUEUE1
+    url = os.environ.get('CLOUDAMQP_URL', 'amqps://ugsynvns:zl-xQ-7yv9OHkXk9s7BQizgUBGBSzTff@fox.rmq.cloudamqp.com/ugsynvns')
+    params = pika.URLParameters(url)
+    connection2 = pika.BlockingConnection(params)
+    channel = connection2.channel() # start a channel
+    channel.queue_declare(queue='queue1')
+    
+    method_frame, header_frame, body = channel.basic_get(queue = 'queue1')
+    #print(body)      
+    channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+    connection2.close() 
+
+    json = json1.loads(body)
+    
+    print(json)
+    
+    sel="SELECT quantity from `stock_management` where book_id='" + json["bid"] + "'"
+    
+    print(sel)
+    mycursor.execute(sel)
+        
+    myresult = mycursor.fetchone()
+    print(myresult)
+    
+    quant=-1
+    cquant=json["quant"]
+    if myresult is not None:
+        if(myresult[0] is not None):
+            quant= myresult[0]
+    print(quant)
+    mssg="nothing"
+    if quant==-1:
+        mssg="product does not exist"
+    elif int(quant)>=int(cquant):
+        mssg="accepted"
+        sel="Update `stock_management` set quantity=" + str(int(quant)-int(cquant)) +" where book_id='" + json["bid"] + "'"
+        mycursor.execute(sel)
+        print('Hii')
+        mydb.commit()
+    else:
+        mssg=" product quantity not sufficint"
+    print(cquant)
+    url = os.environ.get('CLOUDAMQP_URL', 'amqps://ugsynvns:zl-xQ-7yv9OHkXk9s7BQizgUBGBSzTff@fox.rmq.cloudamqp.com/ugsynvns')
+    params = pika.URLParameters(url)
+    connection2 = pika.BlockingConnection(params)
+    channel = connection2.channel() # start a channel
+    channel.queue_declare(queue='queue2')
+    
+    channel.basic_publish(exchange='',
+                          routing_key='queue2',
+                          body=mssg
+                          )
+    connection2.close()
+    print("Order checked !")
+    
+    #UPDATING STOCK AVAILABILITY
+if __name__ == '__main__':
+    app.run(debug=False, port=8000)
+
